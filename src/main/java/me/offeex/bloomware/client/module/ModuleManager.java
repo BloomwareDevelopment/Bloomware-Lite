0c8d1b4cf122374ad9b113c7e0cfe419d24a7f2a
DiOnFire <di0nik.gd@gmail.com>
2022-02-14 22:43:17 +0300

    fixes

--- src/main/java/me/offeex/bloomware/client/module/ModuleManager.java
+++ src/main/java/me/offeex/bloomware/client/module/ModuleManager.java
@@ -3,15 +3,24 @@ package me.offeex.bloomware.client.module;
 import com.google.common.eventbus.Subscribe;
 import me.offeex.bloomware.Bloomware;
 import me.offeex.bloomware.api.event.events.EventKeyPress;
-import me.offeex.bloomware.api.util.ClassUtil;
+import me.offeex.bloomware.client.module.modules.chat.*;
+import me.offeex.bloomware.client.module.modules.client.*;
+import me.offeex.bloomware.client.module.modules.extension.Notifications;
+import me.offeex.bloomware.client.module.modules.extension.RichPresence;
+import me.offeex.bloomware.client.module.modules.extension.StreamerMode;
+import me.offeex.bloomware.client.module.modules.extension.UnfocusedCPU;
+import me.offeex.bloomware.client.module.modules.hud.*;
+import me.offeex.bloomware.client.module.modules.interact.*;
+import me.offeex.bloomware.client.module.modules.motion.*;
+import me.offeex.bloomware.client.module.modules.packet.*;
+import me.offeex.bloomware.client.module.modules.pvp.*;
+import me.offeex.bloomware.client.module.modules.screen.*;
+import me.offeex.bloomware.client.module.modules.visuals.*;
+import me.offeex.bloomware.client.module.modules.world.*;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,20 +29,18 @@ public class ModuleManager {
 
     public ModuleManager() {
         modules = new ArrayList<>();
-        try {
-            ArrayList<Class<?>> classes = ClassUtil.getClassesOther("me.offeex.bloomware.client.module.modules", Module.class);
-            classes.forEach(aClass -> {
-                try {
-                    Module m = (Module) aClass.getDeclaredConstructor().newInstance();
-                    if (getModule(m.getName()) == null) modules.add(m);
-                } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-                    e.printStackTrace();
-                }
-            });
-            modules.sort(Comparator.comparing(Module::getName));
-        } catch (ClassNotFoundException | IOException | URISyntaxException e) {
-            e.printStackTrace();
-        }
+        modules.addAll(List.of(new AntiBots(), new AntiEffect(), new AntiItemLag(), new AntiParticleLag(), new AutoRespawn(), new FakePlayer(), new MobOwner(), new NewChunks(), new NoRender(), new PearlTracker(), new SoundTracker(), new Timer(),
+                new BarrierView(), new BoxHighlight(), new Capes(), new Environment(), new ESP(), new FullBright(), new HoleESP(), new LogoutSpot(), new Nametags(), new StorageESP(), new Tracers(),
+                new BetterScreenshot(), new CameraClip(), new CustomFOV(), new DamageTint(), new Explorer(), new FakeGamemode(), new Freecam(), new RotationLock(), new ViewModel(),
+                new Anchor(), new AutoAnvil(), new AutoCrystal(), new AutoLog(), new AutoTotem(), new Criticals(), new HoleFiller(), new KillAura(), new SelfAnvil(), new SelfWeb(), new Surround(),
+                new Blink(), new FakeVanillaClient(), new MultiConnect(), new PacketCancel(), new PacketLogger(), new ServerCrasher(),
+                new AutoElytra(), new AutoJump(), new AutoWalk(), new BoatFly(), new ElytraFly(), new Flight(), new InventoryMove(), new NoFall(), new NoSlow(), new Speed(), new Sprint(), new Step(), new Velocity(),
+                new AirPlace(), new AntiInteract(), new AutoFish(), new AutoMount(), new BuildHeight(), new FastBreak(), new FastInteract(), new MiddleClick(), new NoGhostBlock(), new Reach(), new Scaffold(), new SilentClose(), new SlotRandomizer(), new XCarry(),
+                new Armor(), new BreakingBlock(), new Coordinates(), new Direction(), new Fps(), new HoleInfo(), new Hunger(), new InventoryViewer(), new LastPacket(), new MemoryUsage(), new ModuleList(), new ModuleNotifier(), new OnlineTime(), new Ping(), new PlayerCount(),
+                new PlayerModel(), new PvPInfo(), new QueuePicker(), new Server(), new Speedometer(), new TabGUI(), new Target(), new TextRadar(), new Time(), new Tps(), new Watermark(), new Welcomer(), new YawPitch(),
+                new Notifications(), new RichPresence(), new StreamerMode(), new UnfocusedCPU(),
+                new Accounts(), new Colors(), new Font(), new Gui(), new Hud(), new MainMenu(), new Overlay(),
+                new AntiSpam(), new ArmorAlert(), new AutoQueueMain(), new BetterChat(), new BetterTab(), new DeathCoords(), new Restarts2b2t(), new Spammer(), new TotemPopCounter(), new VisualRange()));
     }
 
     @Deprecated
